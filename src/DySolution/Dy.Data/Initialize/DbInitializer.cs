8b2abbedd6675aa3d38cf64e08eac053ab666b55
liskying <liwenxue1012@163.com>
2018-01-02 22:03:22 +0800

    代码文件提交

---
+++ src/DySolution/Dy.Data/Initialize/DbInitializer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dy.Data.Initialize
+{
+    /// <summary>
+    /// 数据库自动迁移升级
+    /// </summary>
+    public class DbInitializer
+    {
+        /// <summary>
+        /// 自动升级
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static void InitializeAsync(DyDbContext context)
+        {
+            //获取未应用的Migrations，不必要，MigrateAsync方法会自动处理
+            //var migrations = await context.Database.GetPendingMigrationsAsync();
+            //根据Migrations修改/创建数据库
+            context.Database.MigrateAsync();
+        }
+    }
+}
