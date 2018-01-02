dc18a76d2bd68870f9c9f99b10b46657aee0ce59
liskying <liwenxue1012@163.com>
2018-01-02 22:17:53 +0800

    文件提交

---
+++ src/DySolution/Dy.Service/SysUserManager.cs
@@ -0,0 +1,43 @@
+﻿using Dy.Data.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dy.Service
+{
+    /// <summary>
+    /// 用户管理器
+    /// </summary>
+    public class SysUserManager : AspNetUserManager<SysUser>
+    {
+        /// <summary>
+        /// 构造器
+        /// </summary>
+        /// <param name="store"></param>
+        /// <param name="optionsAccessor"></param>
+        /// <param name="passwordHasher"></param>
+        /// <param name="userValidators"></param>
+        /// <param name="passwordValidators"></param>
+        /// <param name="keyNormalizer"></param>
+        /// <param name="errors"></param>
+        /// <param name="services"></param>
+        /// <param name="logger"></param>
+        public SysUserManager(IUserStore<SysUser> store,
+            IOptions<IdentityOptions> optionsAccessor,
+            IPasswordHasher<SysUser> passwordHasher,
+            IEnumerable<IUserValidator<SysUser>> userValidators,
+            IEnumerable<IPasswordValidator<SysUser>> passwordValidators,
+            ILookupNormalizer keyNormalizer,
+            IdentityErrorDescriber errors,
+            IServiceProvider services,
+            ILogger<UserManager<SysUser>> logger)
+            : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators,
+                 keyNormalizer,
+                 errors, services, logger)
+        { }
+
+    }
+}
---
+++ src/DySolution/Dy.Service/SysUserStore.cs
@@ -0,0 +1,42 @@
+﻿using Dy.Data;
+using Dy.Data.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dy.Service
+{
+    /// <summary>
+    /// 用户存储器
+    /// </summary>
+    public class SysUserStore : UserStore<SysUser, SysRole, DyDbContext, string, SysUserClaim, SysUserRole, SysUserLogin, SysUserToken, SysRoleClaim>
+    {
+        public SysUserStore(DyDbContext context, IdentityErrorDescriber describer = null) : base(context, describer)
+        {
+        }
+
+        /// <summary>
+        /// 查找用户
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public override Task<SysUser> FindByIdAsync(string userId, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Users.Include(u => u.UserDepts)
+                .Include(u => u.UserRoles)
+                .Include(u => u.UserClaims)
+                .Include(u => u.UserLogins)
+                .Include(u => u.UserDepts)
+                .Include(u => u.UserTokens)
+                .FirstOrDefaultAsync(u => u.Id == userId);
+        }
+    }
+}
